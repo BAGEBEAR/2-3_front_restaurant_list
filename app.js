08bf7c0e96be340ec279e45b520ef311220e75e1
bagebear <bagebear@gmail.com>
2021-10-09 18:18:47 +0800

    refactor index and show page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000
 
 const exphbs = require('express-handlebars')
 
-const movieList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -15,7 +15,13 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.send('This is my movie list built with Express')
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() ===
+    req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
 })
 
 // start and listen on the Express server
